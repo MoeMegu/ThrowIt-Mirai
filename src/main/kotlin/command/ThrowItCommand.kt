2bf138dc06ac929740ac51f122aeeca3c884541c
MoeDev <im@moedev.net>
2022-01-31 23:37:07 +0800

    Refactor: throwIt() & CommandSender.sendResult()

--- src/main/kotlin/command/ThrowItCommand.kt
+++ src/main/kotlin/command/ThrowItCommand.kt
@@ -4,23 +4,22 @@ import net.coobird.thumbnailator.Thumbnails
 import net.coobird.thumbnailator.geometry.Positions
 import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.command.CommandSender
-import net.mamoe.mirai.console.command.ConsoleCommandSender.bot
-import net.mamoe.mirai.console.command.ConsoleCommandSender.subject
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.console.util.SemVersion.Companion.satisfies
+import net.mamoe.mirai.contact.BotIsBeingMutedException
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.Image.Key.isUploaded
 import net.mamoe.mirai.message.data.Image.Key.queryUrl
 import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.message.data.sendTo
 import net.mamoe.mirai.utils.ExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
 import pers.moe.ThrowItMirai
-import pers.moe.command.ThrowItCommand.bgImg
 import java.awt.AlphaComposite
 import java.awt.RenderingHints
 import java.awt.geom.Ellipse2D
@@ -48,6 +47,19 @@ object ThrowItCommand : SimpleCommand(
     @Suppress("unused")
     suspend fun CommandSender.handle(target: User) {
         subject ?: throw RuntimeException("请在聊天环境使用此命令")
+        lateinit var result: ExternalResource
+        lateinit var imageMessage: Image
+        try {
+            result = throwIt(ImageIO.read(URL(target.avatarUrl)))
+            result.use { it.close() }
+            imageMessage = sendResult(result, subject!!)
+        } catch (e: IOException) {
+            ThrowItMirai.logger.error(e)
+            ThrowItMirai.logger.error("图片绘制失败")
+        } catch (e: RuntimeException) {
+            ThrowItMirai.logger.error(e)
+            ThrowItMirai.logger.error("图片上传失败")
+        }
         TODO("用户调用")
     }
 
@@ -56,7 +68,6 @@ object ThrowItCommand : SimpleCommand(
     suspend fun CommandSender.handle(image: Image) {
         lateinit var bufferedImage: BufferedImage
         lateinit var result: ExternalResource
-        lateinit var imageMessage: Image
         try {
             bufferedImage = ImageIO.read(URL(image.queryUrl()))
         } catch (e: IOException) {
@@ -66,15 +77,12 @@ object ThrowItCommand : SimpleCommand(
         try {
             result = throwIt(bufferedImage)
             result.use { it.close() }
-            imageMessage = uploadResult(result)
         } catch (e: IOException) {
             ThrowItMirai.logger.error(e)
             ThrowItMirai.logger.error("图片绘制失败")
-        } catch (e: RuntimeException) {
-            ThrowItMirai.logger.error(e)
-            ThrowItMirai.logger.error("图片上传失败")
         }
-        subject!!.sendMessage(imageMessage)
+        sendResult(result)
+
     }
 
     @Handler
@@ -83,7 +91,15 @@ object ThrowItCommand : SimpleCommand(
         TODO("纯文本解析")
 
     }
-
+    /**
+     * 通过传入图片绘制ThrowIt
+     *
+     * @param image 传入一个图片 [BufferedImage] 作为绘制源文件
+     *
+     * @return [ExternalResource] 返回一个外部资源对象作为绘制结果
+     *
+     * @exception IOException 绘制过程中出现异常
+     */
     private fun throwIt(image: BufferedImage): ExternalResource {
         // 设置图层参数
         val avaImg = BufferedImage(image.width, image.height, BufferedImage.TYPE_4BYTE_ABGR)
@@ -126,12 +142,19 @@ object ThrowItCommand : SimpleCommand(
         return result
     }
 
-    private suspend fun uploadResult(result: ExternalResource,subject: Contact): Image {
+    private suspend fun CommandSender.sendResult(result: ExternalResource){
         lateinit var imageMessage: Image
         var failedTry = 0
-        do imageMessage = result.uploadAsImage(subject) while (
-            MiraiConsole.version.satisfies("2.9.0") && !imageMessage.isUploaded(subject.bot) && failedTry++ <= 3
+        do imageMessage = subject!!.uploadImage(result) while (
+            MiraiConsole.version.satisfies("2.9.0") && !imageMessage.isUploaded(bot!!) && failedTry++ <= 3
         )
-        return imageMessage
+        if (failedTry == 3) {
+            ThrowItMirai.logger.error(RuntimeException("Failed uploading message to $subject"))
+            ThrowItMirai.logger.error("ThrowIt:图片上传失败,请检查网络连接")
+        }
+        try {subject!!.sendMessage(imageMessage)} catch (e: BotIsBeingMutedException){
+            ThrowItMirai.logger.error(e)
+            ThrowItMirai.logger.error("ThrowIt:图片发送失败,机器人被禁言")
+        }
     }
 }
